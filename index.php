f07840c7a0fe6aac8fd06594e6e927dc407a76cd
ALautrette <antoine.lautrette@gmail.com>
2023-03-10 10:44:20 +0100

    Router + autoload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+use Route\Router;
+
+spl_autoload_register(function ($class_name) {
+    require($class_name . '.php');
+});
+
+$router = new Router($_GET['url']); 
+$router->get('/', function(){ echo "Bienvenue sur ma homepage !"; }); 
+$router->get('/posts/:id', function($id){ echo "Voila l'article $id"; }); 
+$router->get('/test', "Test#show"); 
+$router->run(); 
