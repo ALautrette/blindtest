43081c597eb148ff6de084b9c329f36b49a1a69a
gdc27 <gabriel27dacosta@gmail.com>
2023-05-23 11:43:31 +0200

    Style auth views

--- App/Views/Auth/reset.php
+++ App/Views/Auth/reset.php
@@ -1,15 +1,38 @@
-<div>
-    <h1>Reset password</h1>
-    <form action="/reset" method="post">
 
-        <label for="email">
-            Email
-        </label>
-        <input type="email" name="email" placeholder="Email" id="email" required>
+<section class="min-vh-100 gradient-custom">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                    <div class="card-body px-sm-5 py-sm-4 text-center">
 
-        <input type="submit" value="Reset">
-    </form>
-    <?php if (isset($message)) : ?>
-        <p><?= $message ?></p>
-    <?php endif; ?>
-</div>
+                        <div class="mt-md-3 pb-5">
+
+                            <h2 class="fw-bold mb-2 text-uppercase">Reset password</h2>
+                            <p class="text-white-50">Please enter your email!</p>
+                            <form action="/reset" method="post">
+
+                                <div class="form-outline form-white mb-3">
+                                    <input placeholder="Email" name="email" type="email" id="typeEmail" class="form-control form-control-lg" required/>
+                                </div>
+
+                                <button class="btn btn-outline-light btn-lg w-100" type="submit">Reset</button>
+                                <?php if (isset($message)) : ?>
+                                    <p><?= $message ?></p>
+                                <?php endif; ?>
+                            </form>
+                        </div>
+
+
+                        <div>
+                            <p class="mb-0">You know your password ? <a href="/login" class="text-white-50 fw-bold">Sign
+                                    In</a>
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- App/Views/Layouts/head.php
+++ App/Views/Layouts/head.php
@@ -3,5 +3,6 @@
     <title>Blindtest</title>
     <link rel="stylesheet" href="/public/style/bootstrap.min.css">
     <link rel="stylesheet" href="/public/style/override.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 </head>
 <body>
