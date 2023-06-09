68a9ab7edcab5e3f5d2c4a2a10cbf476dd0f6f9b
Robin MJ <robin.meunierjacquin@gmail.com>
2023-06-09 12:56:43 +0200

    back arrow add friend

--- App/Views/User/searchForm.php
+++ App/Views/User/searchForm.php
@@ -2,10 +2,12 @@
 include 'App/Views/Layouts/head.php';
 include 'App/Views/Layouts/BackOfficeMenu.php';
 ?>
+<a href="/profile">
+    <i class="fa-solid fa-arrow-left" style="font-size: 40px; margin: 10px 0 0 10px"></i>
+</a>
     <section class="container my-5 bg-dark" style="max-width: 1000px;margin:auto;border-radius: 1rem;">
         <div class="text-white p-3">
             <div>
-
                 <form method="POST" action="/users/search">
                     <label for="username">Username</label>
                     <input class="form-control" type="text" name="username">
